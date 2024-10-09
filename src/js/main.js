cacbc3285c3259f5f3b3d8f2e4f7473b470d9157
Wiktor Łaziński <lazinski.w@wp.pl>
2024-10-09 13:48:11 +0200

    work at js

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,20 @@ const miniNavList = document.querySelector(".mini-nav-list");
 const miniNavItems = miniNavList.querySelectorAll("a");
 const year = document.querySelector(".year");
 const navList = document.querySelector(".nav-list");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#e-mail");
+const textArea = document.querySelector("#msg");
+const popup = document.querySelector(".popup");
+const sendBtn = document.querySelector(".send");
+const clearBtn = document.querySelector(".clear");
+const closeBtn = document.querySelector(".close");
+
+console.log(textArea);
 
 const currentYear = new Date().getFullYear();
 year.textContent = currentYear;
 
+// zarządzanie małą nawigacją
 const showNav = () => {
 	miniNavList.classList.add("show-miniNav");
 	miniClose.style.display = "block";
@@ -50,6 +60,53 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	// Nasłuchuj przewijania strony
 	window.addEventListener("scroll", updateActiveLink);
+});
+
+// wysyłanie wiadomośći
+
+const showError = (elementIn, msg) => {
+	elementIn.parentElement.classList.add("error");
+	elementIn.nextElementSibling.textContent = msg;
+};
+
+const hideError = (elementIn) => {
+	elementIn.parentElement.classList.remove("error");
+};
+
+const checkInput = (elementIn) => {
+	elementIn.forEach((el) => {
+		if (el.value === "") {
+			showError(el, el.placeholder);
+		} else {
+			hideError(el);
+			console.log("wysłano");
+		}
+	});
+};
+
+const checkTextarea = (elementIn) => {
+	if (elementIn.value === "") {
+		elementIn.parentElement.classList.add("error");
+		elementIn.nextElementSibling.textContent = "wiadomość jest pusta";
+	}
+};
+
+sendBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	checkInput([nameInput, emailInput]);
+	checkTextarea(textArea);
+});
+
+// czyszczenie formularza
+
+clearBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	[nameInput, emailInput, textArea].forEach((el) => {
+		el.value = "";
+		el.parentElement.classList.remove("error");
+	});
 });
 
 miniHamburger.addEventListener("click", showNav);
